d5217911a23cf20a1d199d218522229e659a4c41
Priyavarshini-matrixlabs <priyavarshini.sr@matrixlabs.in>
2026-02-16 22:59:56 +0530

    fixed express wildcard error for render deploy

--- server.js
+++ server.js
@@ -158,6 +158,9 @@ app.post("/api/login", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 
 const PORT = process.env.PORT || 3000;
